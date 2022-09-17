17a6775eb43e6d7efa3d5cc22467556c6ff54619
Gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-17 00:37:17 +0000

    [feat] Adding script for the method to retrieve one object in fileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,12 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ method to retrieve one object """
+        if cls is not None or id is not None:
+            query = self.all(cls).values()
+            for value in query:
+                if value.id == id:
+                    return value
+        return None
