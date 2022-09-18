cea07318abcaa403aaf016fb51c708d5a89ac0b7
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-17 22:44:22 -0500

    [feat] Updating script for the create rest full api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,29 +3,9 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
 
 @app_views.route("/status")
 def status():
     """ Route returns "status": "OK" """
     return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats")
-def stats():
-    """ retrieves the number of each objects by type """
-    dict_result = {}
-
-    obj = {"Amenity": "amenities",
-           "City": "cities",
-           "Place": "places",
-           "Review": "reviews",
-           "State": "states",
-           "User": "users"}
-
-    for keys, values in obj.items():
-        count = storage.count(keys)
-        dict_result[values] = count
-
-    return jsonify(dict_result)
