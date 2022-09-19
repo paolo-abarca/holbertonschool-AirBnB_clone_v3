81bcf9a3e37ac0b62d2c91826b7b7733a048df67
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-19 12:36:15 -0500

    Updating create a CORS instance allowing

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
